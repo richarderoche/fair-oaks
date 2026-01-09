d70f0280c790b2a31320fcf2dc132a745e8f02a5
Richard <richard@infinite-productivity.com>
2026-01-09 14:48:21 -0800

    ts

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import SiteWidth from '@/components/shared/SiteWidth'
 import { client } from '@/sanity/client'
 import { homePageQuery } from '@/sanity/queries'
 import { type Home } from '../../sanity.types'
@@ -6,5 +7,17 @@ const options = { next: { revalidate: 30 } }
 export default async function Home() {
   const data = await client.fetch<Home>(homePageQuery, {}, options)
   const { title } = data
-  return <div>{title}</div>
+
+  return (
+    <SiteWidth>
+      <div className="flex flex-col gap-20">
+        <div>{title}</div>
+        <div className="ts-h1">Typestyle H1</div>
+        <div className="ts-h2">Typestyle H2</div>
+        <div className="ts-h3">Typestyle H3</div>
+        <div className="ts-p-lg">Typestyle P MD</div>
+        <div className="ts-p-md">Typestyle P MD</div>
+      </div>
+    </SiteWidth>
+  )
 }
